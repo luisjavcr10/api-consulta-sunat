fdc94435210c0bc3e63d5c782e6ebcdff490a5d1
Luis Javier Castillo <t1053300321@unitru.edu.pe>
2025-02-04 14:06:58 -0500

    debug puppetear version

--- api/utils/handle-puppetear.js
+++ api/utils/handle-puppetear.js
@@ -4,48 +4,31 @@ const puppeteerCore = require('puppeteer-core');
 
 // Inicializa el navegador y la página
 async function initializeBrowser() {
-    console.log("Chromium executable path:", await chromium.executablePath);
-
     let browser;
     try {
-        //if (process.env.AWS_LAMBDA_FUNCTION_VERSION) {
-            // Si está en AWS Lambda o Vercel, usar chrome-aws-lambda
-            browser = await puppeteerCore.launch({
-                executablePath: await chromium.executablePath || '/usr/bin/chromium-browser',
-                args: [
-                    ...chromium.args,
-                    '--no-sandbox',
-                    '--disable-setuid-sandbox',
-                    '--disable-dev-shm-usage',
-                    '--disable-accelerated-2d-canvas',
-                    '--disable-gpu',
-                ],
-                defaultViewport: chromium.defaultViewport,
-                headless: chromium.headless,
-            });
-        //} else {
-        //    // Si está en local, usar Puppeteer normal
-        //    browser = await puppeteer.launch({
-        //        headless: true,
-        //        args: [
-        //            '--no-sandbox',
-        //            '--disable-setuid-sandbox',
-        //            '--disable-dev-shm-usage',
-        //            '--disable-accelerated-2d-canvas',
-        //            '--disable-gpu',
-        //        ],
-        //    });
-        //}
-
-        const page = await browser.newPage();
-        await page.setUserAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36');
-
-        return { browser, page };
+      browser = await puppeteer.launch({
+        executablePath: await chromium.executablePath || "/usr/bin/chromium-browser", // Fallback para entornos locales
+        args: [
+          ...chromium.args,
+          "--no-sandbox",
+          "--disable-setuid-sandbox",
+          "--disable-dev-shm-usage",
+          "--single-process", // Necesario para Vercel
+          "--no-zygote",
+        ],
+        defaultViewport: chromium.defaultViewport,
+        headless: chromium.headless,
+      });
+  
+      const page = await browser.newPage();
+      await page.setUserAgent("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36");
+  
+      return { browser, page };
     } catch (error) {
-        console.error('Error al lanzar Puppeteer:', error);
-        throw new Error('No se pudo iniciar el navegador Puppeteer.');
+      console.error("Error al lanzar Puppeteer:", error);
+      throw new Error("No se pudo iniciar el navegador Puppeteer.");
     }
-}
+  }
 
 
 // Navega a la URL con reintentos
